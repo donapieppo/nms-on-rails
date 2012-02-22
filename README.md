d6aca7cd653c78e1bbd5b5eeff7ef9cfcf1f0b1b
root <root@lennytest.dm.unibo.it>
2012-02-23 00:13:57 +0100

    botstrap and backbone base

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 NMS on Rails
 =================
 
+a basic Network Management System build with
+Ruby on Rails, Backbone and styled with Bootstrap.
+
+## Getting started
+
+After you have cloned and bundle installed...
+
+## Database structure
+
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,10 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require twitter/bootstrap
+//= require underscore
+//= require backbone
+//= require backbone_rails_sync
+//= require backbone_datalink
+//= require backbone/nms_on_rails
 //= require_tree .
