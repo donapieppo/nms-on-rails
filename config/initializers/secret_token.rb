20539135172602e671edda0cfcc00bc58a32e66a
Pietro Donatini <pietro.donatini@unibo.it>
2012-02-24 14:53:03 +0100

    rails standard files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NmsOnRails::Application.config.secret_token = '3e1e00987656e97dc3f58d9764d15e752285cb071c4d9287d5d924c40e057e8a2dd01590ae47a9028f1d09e29f215e11f62f25aed34ca4328ec49d2dee27ac69'
