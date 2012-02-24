23a66ffa55febf4de0c376d461fd7c0f8ad76bbb
Pietro Donatini <pietro.donatini@unibo.it>
2012-02-24 14:48:24 +0100

    attr to change by hand because of backbone behaiour on mass-assignment

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -1,9 +1,13 @@
 class InfosController < ApplicationController
   respond_to :json
 
+  # FIXME backbone sends a complete representation of the resource on save.
+  # It breaks attr_accessible in model. So we need to clean up.
   def create
     @ip = Ip.find(params[:ip_id] || params[:info][:ip_id])
-    @info = @ip.infos.new(params[:info])
+    @info = @ip.infos.new()
+    @info.name    = params[:info][:name]
+    @info.comment = params[:info][:comment]
     if @info.save
       render :json => @info
     else
@@ -12,9 +16,10 @@ class InfosController < ApplicationController
   end
 
   def update
-    @info = Info.find(params.delete(:id))
-    params[:info].delete(:id)
-    if @info.update_attributes(params[:info])
+    @info = Info.find(params[:id])
+    @info.name    = params[:info][:name] 
+    @info.comment = params[:info][:comment]
+    if @info.save
       render :json => @info
     else
       render :json => @info.errors, :status => :unprocessable_entity 
