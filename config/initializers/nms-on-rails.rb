616072fdb11d0df72654cbac339a378889124efe
Pietro Donatini <pietro.donatini@unibo.it>
2012-03-08 11:55:15 +0100

    configurations

--- config/initializers/nms-on-rails.rb
+++ config/initializers/nms-on-rails.rb
@@ -1,7 +1,14 @@
-NmsOnRails::Application.configure do 
-  # Arpwatch
-  config.arpwatch_files = ['/var/tmp/arp.dat']
-  # Facts
+module NmsOnRails
+  class Application < Rails::Application
+    config.before_initialize do 
+      # Domains with server where you can get dig axfr
+      config.dns_domains = {'dm.unibo.it'    => '137.204.134.1', 
+                            'ciram.unibo.it' => '137.204.132.20'}
+      # Arpwatch
+      config.arpwatch_files = ['/var/lib/arpwatch/arp.dat', '/home/donatini/arp_ciram.dat']
+      # Facts
+    end
+  end
 end
 
 
