f58893cd3c5028ad75fb238b5b50165d12efe9d5
Pietro Donatini <pietro.donatini@unibo.it>
2012-04-19 15:01:35 +0200

    switches

---
+++ app/controllers/switches_controller.rb
@@ -0,0 +1,37 @@
+class SwitchesController < ApplicationController
+  def index
+    @switches = Switch.all
+  end
+
+  def new
+    @switch = Switch.new
+  end
+
+  def edit
+    @switch = Switch.find(params[:id])
+  end
+
+  def create
+    @switch = Switch.new(params[:switch])
+    if @switch.save
+      redirect_to(@switch, :notice => 'Switch was successfully created.') 
+    else
+      render :action => "new" 
+    end
+  end
+
+  def update
+    @switch = Switch.find(params[:id])
+    if @switch.update_attributes(params[:switch])
+      redirect_to(@switch, :notice => 'Switch was successfully updated.') 
+    else
+      render :action => "edit" 
+    end
+  end
+
+  def destroy
+    @switch = Switch.find(params[:id])
+    @switch.destroy
+    redirect_to switchs_path
+  end
+end
