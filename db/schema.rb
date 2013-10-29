246295f4842883a0e69a03b54dc0bb50bafa3c9b
Pietro Donatini <donatini@dm.unibo.it>
2013-10-29 13:18:44 +0100

    days ago  for facts

--- db/schema.rb
+++ db/schema.rb
@@ -9,74 +9,74 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "arps", :force => true do |t|
-    t.integer   "ip_id",               :default => 0, :null => false
-    t.string    "mac",   :limit => 17
-    t.timestamp "date",                               :null => false
+  create_table "arps", force: true do |t|
+    t.integer   "ip_id",            default: 0, null: false
+    t.string    "mac",   limit: 17
+    t.timestamp "date",                         null: false
   end
 
-  add_index "arps", ["ip_id", "mac", "date"], :unique => true
+  add_index "arps", ["ip_id", "mac", "date"], name: "ip_id", unique: true, using: :btree
 
-  create_table "facts", :force => true do |t|
-    t.integer "ip_id",                         :null => false
-    t.integer "memorysize"
-    t.string  "processor",      :limit => 50
-    t.integer "processorcount"
-    t.string  "lsbdistrelease", :limit => 20
-    t.string  "lsbdistid",      :limit => 20
-    t.string  "kernelrelease",  :limit => 100
+  create_table "facts", force: true do |t|
+    t.integer   "ip_id",                      null: false
+    t.integer   "memorysize"
+    t.string    "processor",      limit: 50
+    t.integer   "processorcount"
+    t.string    "lsbdistrelease", limit: 20
+    t.string    "lsbdistid",      limit: 20
+    t.string    "kernelrelease",  limit: 100
+    t.timestamp "date",                       null: false
   end
 
-  create_table "infos", :force => true do |t|
-    t.integer   "ip_id",   :default => 0, :null => false
-    t.timestamp "date",                   :null => false
+  create_table "infos", force: true do |t|
+    t.integer   "ip_id",   default: 0, null: false
+    t.timestamp "date",                null: false
     t.text      "name"
     t.text      "dnsname"
     t.text      "comment"
-    t.integer   "user_id", :default => 0, :null => false
+    t.integer   "user_id", default: 0, null: false
     t.boolean   "dhcp"
   end
 
-  add_index "infos", ["ip_id", "date"], :unique => true
+  add_index "infos", ["ip_id", "date"], name: "ip_id", unique: true, using: :btree
 
-  create_table "ips", :force => true do |t|
-    t.string  "ip",           :limit => 15, :default => "", :null => false
+  create_table "ips", force: true do |t|
+    t.string  "ip",           limit: 15, default: "", null: false
     t.integer "last_arp_id"
     t.integer "last_info_id"
-    t.string  "conn_proto",   :limit => 5
+    t.string  "conn_proto",   limit: 5
     t.boolean "notify"
     t.integer "network_id"
   end
 
-  add_index "ips", ["ip"], :unique => true
+  add_index "ips", ["ip"], name: "ip", unique: true, using: :btree
 
-  create_table "networks", :force => true do |t|
-    t.string "name", :limit => 30
-    t.string "description", :limit => 250
+  create_table "networks", force: true do |t|
+    t.string "name", limit: 250
   end
 
-  create_table "ports", :force => true do |t|
+  create_table "ports", force: true do |t|
     t.integer  "switch_id"
     t.integer  "port"
-    t.string   "mac",       :limit => 17
+    t.string   "mac",       limit: 17
     t.datetime "date"
   end
 
-  add_index "ports", ["mac"], :name => "index_ports_on_mac"
-  add_index "ports", ["switch_id"], :name => "index_ports_on_switch_id"
+  add_index "ports", ["mac"], name: "index_ports_on_mac", using: :btree
+  add_index "ports", ["switch_id"], name: "index_ports_on_switch_id", using: :btree
 
-  create_table "switches", :force => true do |t|
-    t.string "ip",        :limit => 20
-    t.string "name",      :limit => 250
-    t.string "community", :limit => 100
+  create_table "switches", force: true do |t|
+    t.string "ip",        limit: 20
+    t.string "name",      limit: 250
+    t.string "community", limit: 100
   end
 
-  create_table "users", :force => true do |t|
-    t.string "login", :limit => 15
+  create_table "users", force: true do |t|
+    t.string "login", limit: 15
     t.text   "gecos"
   end
 
