0736f1e97471d2fccc35c7cf87086bc552a8302c
Pietro Donatini <pietro.donatini@unibo.it>
2013-12-16 14:48:03 +0100

    number of ports in switch

--- app/models/switch.rb
+++ app/models/switch.rb
@@ -1,3 +1,12 @@
 class Switch < ActiveRecord::Base
   has_many :ports
+
+  def number_of_ports
+    clean_ip = Ip.clean!(self.ip)
+    shell_command = "snmpwalk -On -v 2c -c #{self.community} #{clean_ip} .1.3.6.1.2.1.17.2.7.0"
+    line = IO.popen(shell_command).readline
+    line =~ /.1.3.6.1.2.1.17.2.7.0 = INTEGER: (\d+)/ or raise "wrong format for number of port in switch"
+    $1.to_i
+  end
+  
 end
