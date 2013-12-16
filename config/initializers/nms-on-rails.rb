129cb718ea60f902d99dd88eb082df26cf0a40c2
Pietro Donatini <pietro.donatini@unibo.it>
2013-12-16 14:50:20 +0100

    config file

--- config/initializers/nms-on-rails.rb
+++ config/initializers/nms-on-rails.rb
@@ -3,12 +3,14 @@ module NmsOnRails
     config.before_initialize do 
       # Domains with server where you can get dig axfr
       # on your rails server should work `dig axfr dm.unibo.it`
-      config.dns_domains = {'dm.unibo.it'    => '137.204.134.1', 
-                            'ciram.unibo.it' => '137.204.132.20'}
+      config.dns_domains = {'example.com'    => '192.168.1.1', 
+                            'example2.com'   => '192.168.2.1'}
       # Arpwatch
-      config.arpwatch_files = ['/var/lib/arpwatch/arp.dat', '/var/lib/arpwatch/arp_ciram.dat']
+      config.arpwatch_files = ['/var/lib/arpwatch/arp.dat', '/tmp/arp_other.dat']
       # Facts
       config.facts_dir = "/var/lib/puppet/yaml/facts"
+      # snmpwalk
+      config.snmpwalkexe = '/usr/bin/snmpwalk'
     end
   end
 end
