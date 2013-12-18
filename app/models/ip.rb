06efcd519f0d24d4b7988e5e52b1368d5db287c3
Pietro Donatini <pietro.donatini@unibo.it>
2013-12-18 11:51:01 +0100

    clean

--- app/models/ip.rb
+++ app/models/ip.rb
@@ -13,13 +13,21 @@ class Ip < ActiveRecord::Base
   validates :ip, :format => { :with => IP_REGEXP, :message => "wrong ip format" }
   validates :conn_proto, :inclusion => { :in => %w(ssh rdp http), :message => "%{value} is not a valid protocol", :allow_nil => true }
 
+  def last_arp
+    self.arps.order('date desc').first
+  end
+
+  def last_info
+    self.infos.order('date desc').first
+  end
+
   def update_last_arp
-    last = self.arps.order('date desc').first
+    last = self.last_arp
     last and self.update_attribute(:last_arp_id, last.id)
   end
 
   def update_last_info
-    last = self.infos.order('date desc').first
+    last = self.last_info
     last and self.update_attribute(:last_info_id, last.id)
   end
 
