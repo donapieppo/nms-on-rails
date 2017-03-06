a057d229bf1f07bfa3f2e824e0d62f86c16c808d
Pietro <donapieppo@yahoo.it>
2017-03-06 11:51:38 +0100

    rails 5.1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def horizontal_form_for(record, options={}, &block)
-    simple_form_for(record, options.merge({html: { class: 'form-horizontal' }, wrapper: :horizontal_form}), &block)
-  end
 end
