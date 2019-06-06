b555dc12568f9c2a34c40717a593e2e01c93f558
Donapieppo <donapieppo@yahoo.it>
2019-06-06 13:11:12 +0200

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,94 +1,88 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 0) do
 
-  create_table "arps", force: true do |t|
-    t.integer   "ip_id",            default: 0, null: false
-    t.string    "mac",   limit: 17
-    t.timestamp "date",                         null: false
+  create_table "arps", id: :integer, unsigned: true, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "ip_id", default: 0, null: false, unsigned: true
+    t.string "mac", limit: 17
+    t.timestamp "date", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.index ["ip_id", "mac", "date"], name: "ip_id", unique: true
   end
 
-  add_index "arps", ["ip_id", "mac", "date"], name: "ip_id", unique: true, using: :btree
-
-  create_table "facts", force: true do |t|
-    t.integer   "ip_id",                      null: false
-    t.integer   "memorysize"
-    t.string    "processor",      limit: 50
-    t.integer   "processorcount"
-    t.string    "lsbdistrelease", limit: 20
-    t.string    "lsbdistid",      limit: 20
-    t.string    "kernelrelease",  limit: 100
-    t.timestamp "date",                       null: false
+  create_table "facts", id: :integer, unsigned: true, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "ip_id", null: false, unsigned: true
+    t.integer "memorysize"
+    t.string "processor", limit: 50
+    t.integer "processorcount"
+    t.string "lsbdistrelease", limit: 20
+    t.string "lsbdistid", limit: 20
+    t.string "kernelrelease", limit: 100
+    t.timestamp "date", default: -> { "CURRENT_TIMESTAMP" }, null: false
   end
 
-  create_table "infos", force: true do |t|
-    t.integer   "ip_id",   default: 0, null: false
-    t.timestamp "date",                null: false
-    t.text      "name"
-    t.text      "dnsname"
-    t.text      "comment"
-    t.integer   "user_id", default: 0, null: false
-    t.boolean   "dhcp"
+  create_table "infos", id: :integer, unsigned: true, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "ip_id", default: 0, null: false, unsigned: true
+    t.timestamp "date", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.text "name"
+    t.text "dnsname"
+    t.text "comment"
+    t.integer "user_id", default: 0, null: false, unsigned: true
+    t.boolean "dhcp"
+    t.index ["ip_id", "date"], name: "ip_id", unique: true
   end
 
-  add_index "infos", ["ip_id", "date"], name: "ip_id", unique: true, using: :btree
-
-  create_table "ips", force: true do |t|
-    t.string  "ip",             limit: 15, default: "", null: false
-    t.integer "last_arp_id"
-    t.integer "last_info_id"
-    t.string  "conn_proto",     limit: 5
+  create_table "ips", id: :integer, unsigned: true, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "ip", limit: 15, default: "", null: false
+    t.integer "last_arp_id", unsigned: true
+    t.integer "last_info_id", unsigned: true
+    t.string "conn_proto", limit: 5
     t.boolean "notify"
     t.integer "network_id"
-    t.integer "last_system_id"
+    t.integer "last_system_id", unsigned: true
+    t.index ["ip"], name: "ip", unique: true
   end
 
-  add_index "ips", ["ip"], name: "ip", unique: true, using: :btree
-
-  create_table "networks", force: true do |t|
-    t.string "name",        limit: 250
-    t.text   "description"
+  create_table "networks", id: :integer, options: "ENGINE=MyISAM DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.string "name", limit: 250
+    t.text "description"
   end
 
-  create_table "ports", force: true do |t|
-    t.integer  "switch_id"
-    t.integer  "port"
-    t.string   "mac",       limit: 17
+  create_table "ports", id: :integer, options: "ENGINE=MyISAM DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.integer "switch_id"
+    t.integer "port"
+    t.string "mac", limit: 17
     t.datetime "last"
     t.datetime "start"
+    t.index ["mac"], name: "index_ports_on_mac"
+    t.index ["switch_id"], name: "index_ports_on_switch_id"
   end
 
-  add_index "ports", ["mac"], name: "index_ports_on_mac", using: :btree
-  add_index "ports", ["switch_id"], name: "index_ports_on_switch_id", using: :btree
-
-  create_table "switches", force: true do |t|
-    t.string "ip",        limit: 20
-    t.string "name",      limit: 250
+  create_table "switches", id: :integer, options: "ENGINE=MyISAM DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.string "ip", limit: 20
+    t.string "name", limit: 250
     t.string "community", limit: 100
   end
 
-  create_table "systems", force: true do |t|
-    t.integer   "ip_id", null: false
-    t.string    "name"
-    t.timestamp "date",  null: false
+  create_table "systems", id: :integer, unsigned: true, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "ip_id", null: false, unsigned: true
+    t.string "name"
+    t.timestamp "date", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.index ["ip_id"], name: "index_oss_on_ip_id"
   end
 
-  add_index "systems", ["ip_id"], name: "index_oss_on_ip_id", using: :btree
-
-  create_table "users", force: true do |t|
+  create_table "users", id: :integer, default: 0, unsigned: true, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "login", limit: 15
-    t.text   "gecos"
+    t.text "gecos"
   end
 
 end
