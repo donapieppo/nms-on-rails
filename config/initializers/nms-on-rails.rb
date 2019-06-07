cfb253e5a3b11b5050a083c4f719ce74a37c8b10
Donapieppo <donapieppo@yahoo.it>
2019-06-07 13:10:09 +0200

    delete

--- config/initializers/nms-on-rails.rb
+++ config/initializers/nms-on-rails.rb
@@ -1,6 +1,5 @@
 # rsync -av root@newton.ciram.unibo.it:/var/lib/arpwatch/arp.dat /home/tmp/ciram_arp.dat
 # rsync -av root@dmpuppet:/var/lib/arpwatch/arp.dat /home/tmp
-# i
 module NmsOnRails
   class Application < Rails::Application
     config.before_initialize do 
