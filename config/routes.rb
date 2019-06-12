8f7ed24ac2bbe5a4ae7b1f0bc358cb837db35aa3
Donapieppo <donapieppo@yahoo.it>
2019-06-12 10:31:21 +0200

    style

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :ips do
-    put :notify,  :on => :member
-    put :reset,   :on => :member
-    get :connect, :on => :member
-    get :wake,    :on => :member
+    put :notify,  on: :member
+    put :reset,   on: :member
+    get :connect, on: :member
+    get :wake,    on: :member
     resource :infos
     resource :facts
   end
@@ -18,5 +18,5 @@ Rails.application.routes.draw do
   resources :switches
 
   get 'macs/:address' => 'macs#show'
-  root :to => 'home#index'
+  root to: 'home#index'
 end
