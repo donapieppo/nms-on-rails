ddaea9c3177a61f22266bf1ed3db9fe2e146a1be
Donapieppo <donapieppo@yahoo.it>
2019-06-12 10:28:59 +0200

    bug after_save and not after_create

--- app/models/system.rb
+++ app/models/system.rb
@@ -1,7 +1,7 @@
 class System < ApplicationRecord
   belongs_to :ip
 
-  after_create :update_ip_last_system
+  after_save :update_ip_last_system
 
   def update_ip_last_system
     self.ip.update_last_system
