fbfcfb2eaa287ae69a07aa5c9f874b60c1a1bfed
Donapieppo <donapieppo@yahoo.it>
2019-06-19 15:11:54 +0200

    todo facts

--- app/controllers/facts_controller.rb
+++ app/controllers/facts_controller.rb
@@ -1,10 +1,6 @@
 class FactsController < ApplicationController
-  #respond_to :html
-  #respond_to :json, :only => [:show]
-  #respond_to :json
-
   def index
-    @facts = Fact.includes(:ip => [:info, :arp]).order(:lsbdistrelease, 'ips.ip')
+    @facts = Fact.includes(ip: [:info, :arp]).order(:lsbdistrelease, 'ips.ip')
     respond_to do |format|
       format.html
       format.json { render json: client_json(@facts) }
@@ -16,8 +12,11 @@ class FactsController < ApplicationController
       @ip = Ip.find(params[:ip_id])
     end
 
-    @fact = @ip ? Fact.where(:ip_id => @ip.id).first : Fact.find(params[:id])
-    respond_with(@fact)
+    @fact = @ip ? Fact.where(ip_id: @ip.id).first : Fact.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: client_json(@fact) }
+    end
   end
 
   private 
